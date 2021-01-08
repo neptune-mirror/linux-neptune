f541c7bf7ed5890b0b9a1df1d1dd88e0f34d2ce1
Lakshmipathi <lakshmipathi.ganapathi@collabora.com>
2021-01-08 21:28:35 +0530

    Build kernel
    Signed-off-by: Lakshmipathi <lakshmipathi.ganapathi@collabora.com>

--- kernel_test.sh
+++ kernel_test.sh
@@ -18,9 +18,13 @@ echo "========================================================================"
 
 echo -e "\t\t\t\t Build Perf benchmark"
 echo "========================================================================" 
+cd /mnt
+make headers_install INSTALL_HDR_PATH=/usr
+echo "grep FUTEX_32"
+grep FUTEX_32 /usr/include/linux/futex.h
 apt-get update && apt-get install  elfutils libunwind-dev binutils numactl libaudit-dev coreutils libelf-dev libzstd-dev libcap-dev -y
-apt-get install -y flex bison build-essential
-cd /mnt && make -C tools/perf -f tests/make
+apt-get update && apt-get install -y flex bison build-essential libiberty-dev libbabeltrace-ctf-dev libperl-dev libslang2-dev libssl-dev systemtap-sdt-dev libdw-dev
+# cd /mnt && make -C tools/perf -f tests/make
 cd /mnt/tools/perf/ && make
 
 echo -e "\t\t\t\t Completed perf benchmark build" 
