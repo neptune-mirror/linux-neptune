6c788c774106700fe2d627cb4cb1c7e980d5a7f7
Lucas Tanure <tanureal@opensource.cirrus.com>
2021-07-22 14:15:53 +0100

    spi: core: Add flag for controllers that can't hold cs between transfers
    Create a flag for a controller that has an automatic cs selection and
    can't hold cs activated between transfers
    Some messages send address and data split between two transfers, see
    regmap-spi, and without the cs held the data loses it`s meaning
    
    Change-Id: Id4eb8e8b8524be2d08edf54cc0caa6639102978e
    Signed-off-by: Lucas Tanure <tanureal@opensource.cirrus.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -511,6 +511,7 @@ struct spi_controller {
 #define SPI_CONTROLLER_MUST_TX		BIT(4)	/* requires tx */
 
 #define SPI_MASTER_GPIO_SS		BIT(5)	/* GPIO CS must select slave */
+#define SPI_CONTROLLER_CS_PER_TRANSFER	BIT(6)	/* SPI controller can not hold CS between transfers */
 
 	/* flag indicating this is an SPI slave controller */
 	bool			slave;
