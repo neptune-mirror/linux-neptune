9200ca4845a6c8a122b8af81b955e27b5397dd6c
Andrey Smirnov <andrew.smirnov@gmail.com>
2022-02-05 14:30:29 -0800

    usb: dwc3: Drop "usb-role-switch" from AMD property list
    Needed to use extcon.

--- drivers/usb/dwc3/dwc3-pci.c
+++ drivers/usb/dwc3/dwc3-pci.c
@@ -165,7 +165,6 @@ static const struct software_node dwc3_pci_amd_swnode = {
 
 static const struct property_entry dwc3_pci_amd_mr_properties[] = {
 	PROPERTY_ENTRY_STRING("dr_mode", "otg"),
-	PROPERTY_ENTRY_BOOL("usb-role-switch"),
 	PROPERTY_ENTRY_STRING("role-switch-default-mode", "host"),
 	PROPERTY_ENTRY_BOOL("linux,sysdev_is_parent"),
 	PROPERTY_ENTRY_STRING("linux,extcon-name", "VLV0100:00"),
