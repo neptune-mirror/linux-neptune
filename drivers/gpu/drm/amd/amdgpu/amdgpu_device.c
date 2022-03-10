1b02c00bda0933413ba4e9267635df69117fb48d
Somalapuram Amaranath <Amaranath.Somalapuram@amd.com>
2022-03-10 11:31:44 +0530

    drm/amdgpu: schedule GPU reset event work function
    Schedule work function with valid PID, process name,
    and vram lost status during a GPU reset/ recovery.
    
    Signed-off-by: Somalapuram Amaranath <Amaranath.Somalapuram@amd.com>
    (cherry picked from commit 293c019a84c6402b08db9579819b555b01cd613b)
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4713,6 +4713,19 @@ int amdgpu_do_asic_reset(struct list_head *device_list_handle,
 					goto out;
 
 				vram_lost = amdgpu_device_check_vram_lost(tmp_adev);
+				if (reset_context->job && reset_context->job->vm) {
+					tmp_adev->reset_event_info.pid =
+						reset_context->job->vm->task_info.pid;
+					memset(tmp_adev->reset_event_info.pname, 0, TASK_COMM_LEN);
+					strcpy(tmp_adev->reset_event_info.pname,
+						reset_context->job->vm->task_info.process_name);
+				} else {
+					tmp_adev->reset_event_info.pid = 0;
+					memset(tmp_adev->reset_event_info.pname, 0, TASK_COMM_LEN);
+				}
+
+				tmp_adev->reset_event_info.flags = vram_lost;
+				schedule_work(&tmp_adev->gpu_reset_event_work);
 				if (vram_lost) {
 					DRM_INFO("VRAM is lost due to GPU reset!\n");
 					amdgpu_inc_vram_lost(tmp_adev);
