b650e2b391a100d84f9c5fd05f58ab4893286de2
Simon Ser <contact@emersion.fr>
2022-08-30 17:29:26 +0000

    drm: introduce drm_mode_config.atomic_async_page_flip_not_supported
    This new field indicates whether the driver has the necessary logic
    to support async page-flips via the atomic uAPI. This is leveraged by
    the next commit to allow user-space to use this functionality.
    
    All atomic drivers setting drm_mode_config.async_page_flip are updated
    to also set drm_mode_config.atomic_async_page_flip_not_supported. We
    will gradually check and update these drivers to properly handle
    drm_crtc_state.async_flip in their atomic logic.
    
    The goal of this negative flag is the same as
    fb_modifiers_not_supported: we want to eventually get rid of all
    drivers missing atomic support for async flips. New drivers should not
    set this flag, instead they should support atomic async flips (if
    they support async flips at all). IOW, we don't want more drivers
    with async flip support for legacy but not atomic.
    
    v2: only set the flag on atomic drivers (remove it on amdgpu DCE and
    on radeon)
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Joshua Ashton <joshua@froggi.es>
    Cc: Melissa Wen <mwen@igalia.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Harry Wentland <hwentlan@amd.com>
    Cc: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Cc: André Almeida <andrealmeid@igalia.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    [Fwd-ported to v6.5]
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Link: https://lore.kernel.org/r/20220830172851.269402-4-contact@emersion.fr

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -1068,6 +1068,7 @@ int vc4_kms_load(struct drm_device *dev)
 	dev->mode_config.helper_private = &vc4_mode_config_helpers;
 	dev->mode_config.preferred_depth = 24;
 	dev->mode_config.async_page_flip = true;
+	dev->mode_config.atomic_async_page_flip_not_supported = true;
 	dev->mode_config.normalize_zpos = true;
 
 	ret = vc4_ctm_obj_init(vc4);
