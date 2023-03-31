7bdc6422c560abb73245493c9d2c41483df7457b
Joshua Ashton <joshua@froggi.es>
2023-03-31 10:51:40 +0100

    DISABLE color_mgmt_changed

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_color.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_color.c
@@ -909,7 +909,7 @@ int amdgpu_dm_update_plane_color_mgmt(struct dm_crtc_state *crtc,
 		dc_plane_state->in_transfer_func->tf = TRANSFER_FUNCTION_LINEAR;
 	}
 
-	if (plane_state->color_mgmt_changed) {
+	//if (plane_state->color_mgmt_changed) {
 		const struct drm_color_lut *shaper_lut, *lut3d;
 		uint32_t lut3d_size, shaper_size;
 		enum drm_transfer_function shaper_tf = DRM_TRANSFER_FUNCTION_DEFAULT;
