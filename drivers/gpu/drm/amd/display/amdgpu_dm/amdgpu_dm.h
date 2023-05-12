7948ab13f30faa5b12a2a041986090870ac1eaba
Joshua Ashton <joshua@froggi.es>
2023-05-12 18:43:46 +0000

    drm/amd/display: Reset CRTC color mgmt state if commit fails
    If we failed our commit, if our new crtc was dirty color-wise, make
    sure we forward that dirty bit on our old state now so we don't
    forget to revert the work we did before we failed this commit on
    the next commit.
    
    Fixes a bug in SteamOS/Gamescope where we could fail a
    commit that changed the gamma of the CRTC and it would
    persist even after we did a new commit with a different
    gamma (as we were doing single plane optimizations
    and didn't need degamma + gamma for correct blending)
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -727,6 +727,7 @@ struct dm_crtc_state {
 	struct dc_info_packet vrr_infopacket;
 
 	int abm_level;
+	bool cm_needs_reset;
 };
 
 #define to_dm_crtc_state(x) container_of(x, struct dm_crtc_state, base)
