db19a568d4465c718b03a7a839872ba7bbb48ef0
Melissa Wen <mwen@igalia.com>
2023-06-12 06:31:18 -0100

    draft: drm/amd/display: add plane CTM driver-specific property
    Plane CTM for color space conversion.
    
    Signed-off-by: Melissa Wen <mwen@igalia.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1306,6 +1306,13 @@ amdgpu_display_create_color_properties(struct amdgpu_device *adev)
 
 	prop = drm_property_create(adev_to_drm(adev),
 				   DRM_MODE_PROP_BLOB,
+				   "VALVE1_PLANE_CTM", 0);
+	if (!prop)
+		return -ENOMEM;
+	adev->mode_info.plane_ctm_property = prop;
+
+	prop = drm_property_create(adev_to_drm(adev),
+				   DRM_MODE_PROP_BLOB,
 				   "VALVE1_PLANE_SHAPER_LUT", 0);
 	if (!prop)
 		return -ENOMEM;
