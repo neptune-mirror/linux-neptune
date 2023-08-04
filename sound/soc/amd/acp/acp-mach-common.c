41197b693b30eeefcf0e4af1fc67991f6416f0de
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2023-08-04 17:47:17 +0530

    ASoC: amd: acp: Modify nau8821 dapm widget name
    Modify nau8821 dapm widget name in acp common machine driver
    to fix below warning.
    
    debugfs: File 'Headset Mic' in directory 'dapm' already present!
    
    Fixes: 4b526b3278be (ASoC: amd: acp: Add machine driver support for nau8821 codec)
    
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>

--- sound/soc/amd/acp/acp-mach-common.c
+++ sound/soc/amd/acp/acp-mach-common.c
@@ -923,7 +923,7 @@ static int platform_clock_control(struct snd_soc_dapm_widget *w,
 
 static const struct snd_soc_dapm_widget nau8821_widgets[] = {
 	SND_SOC_DAPM_HP("Headphone jack", NULL),
-	SND_SOC_DAPM_MIC("Headset Mic", NULL),
+	SND_SOC_DAPM_MIC("Headset mic", NULL),
 	SND_SOC_DAPM_MIC("Int Mic", NULL),
 	SND_SOC_DAPM_SUPPLY("Platform Clock", SND_SOC_NOPM, 0, 0,
 			    platform_clock_control, SND_SOC_DAPM_PRE_PMU |
@@ -934,11 +934,11 @@ static const struct snd_soc_dapm_route nau8821_audio_route[] = {
 	/* HP jack connectors - unknown if we have jack detection */
 	{ "Headphone jack", NULL, "HPOL" },
 	{ "Headphone jack", NULL, "HPOR" },
-	{ "MICL", NULL, "Headset Mic" },
-	{ "MICR", NULL, "Headset Mic" },
+	{ "MICL", NULL, "Headset mic" },
+	{ "MICR", NULL, "Headset mic" },
 	{ "DMIC", NULL, "Int Mic" },
 	{ "Headphone jack", NULL, "Platform Clock" },
-	{ "Headset Mic", NULL, "Platform Clock" },
+	{ "Headset mic", NULL, "Platform Clock" },
 	{ "Int Mic", NULL, "Platform Clock" },
 };
 
