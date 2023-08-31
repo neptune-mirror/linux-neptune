2511f8906346400a4d4a237c40e484ed99a23a72
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2023-08-31 17:33:12 +0530

    ASoC: SOF: amd: Add spinlock in irq thread
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>

--- sound/soc/sof/amd/acp.c
+++ sound/soc/sof/amd/acp.c
@@ -352,6 +352,7 @@ static irqreturn_t acp_irq_thread(int irq, void *context)
 	const struct sof_amd_acp_desc *desc = get_chip_info(sdev->pdata);
 	unsigned int count = ACP_HW_SEM_RETRY_COUNT;
 
+	spin_lock_irq(&sdev->ipc_lock);
 	while (snd_sof_dsp_read(sdev, ACP_DSP_BAR, desc->hw_semaphore_offset)) {
 		/* Wait until acquired HW Semaphore lock or timeout */
 		count--;
@@ -364,6 +365,7 @@ static irqreturn_t acp_irq_thread(int irq, void *context)
 	sof_ops(sdev)->irq_thread(irq, sdev);
 	/* Unlock or Release HW Semaphore */
 	snd_sof_dsp_write(sdev, ACP_DSP_BAR, desc->hw_semaphore_offset, 0x0);
+	spin_unlock_irq(&sdev->ipc_lock);
 
 	return IRQ_HANDLED;
 };
