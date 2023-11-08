234d6d21b9eda7fba368e6423626db4bd04e4afd
Pierre-Loup A. Griffais <pgriffais@valvesoftware.com>
2023-11-07 16:57:15 -0800

    Revert "Galileo-only workaround for backlight settings"
    This reverts commit f1f63fbd6a31efad6165f4b35b20ba65f25f877b.

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -147,7 +147,7 @@ MODULE_FIRMWARE(FIRMWARE_NAVI12_DMCU);
 #define PSP_FOOTER_BYTES 0x100
 
 /* Maximum backlight level. */
-#define AMDGPU_MAX_BL_LEVEL 0xFFF
+#define AMDGPU_MAX_BL_LEVEL 0xFFFF
 
 /**
  * DOC: overview
@@ -4123,12 +4123,9 @@ static u32 convert_brightness_from_user(const struct amdgpu_dm_backlight_caps *c
 {
 	unsigned min, max;
 
-	//if (!get_brightness_range(caps, &min, &max))
-	//	return brightness;
-        min = 0;
-	max = 500000;
+	if (!get_brightness_range(caps, &min, &max))
+		return brightness;
 
-	DRM_INFO("[%s, %d] brightness range %d - %d", __func__, __LINE__, min, max);
 	// Rescale 0..AMDGPU_MAX_BL_LEVEL to min..max
 	return min + DIV_ROUND_CLOSEST((max - min) * brightness,
 				       AMDGPU_MAX_BL_LEVEL);
