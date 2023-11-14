b7afc7bca5e78a96d9aea137af3f0a27f160b7ee
John Schoenick <johns@valvesoftware.com>
2023-11-13 18:17:21 -0800

    ASoC: amd: acp: Restore missing cs35l41 configuration
    Galilypad: The following commits pulled in a new version of this that
    omitted the config needed for jupiter boards with the cs35l41 codec.
    This restores the missing sections.  This should be folded into the
    final upstreaming of the galileo patchset
    
    08507ab4a662 ASoC: amd: Add acpi machine id's for vangogh platform
    7c7ae3a5eecd Revert "ASoC: SOF: amd: Add acpi machine id's for vangogh
    platform"

--- sound/soc/amd/acp-config.c
+++ sound/soc/amd/acp-config.c
@@ -110,9 +110,12 @@ static struct snd_soc_acpi_codecs amp_max = {
 	.codecs = {"MX98360A"}
 };
 
-static struct snd_soc_acpi_codecs amp_max98388 = {
-	.num_codecs = 1,
-	.codecs = {"ADS8388"}
+static struct snd_soc_acpi_codecs amp_max98388 = { .num_codecs = 1,
+						   .codecs = { "ADS8388" } };
+
+static struct snd_soc_acpi_codecs amp_cs35l41 = {
+   .num_codecs = 1,
+   .codecs = {"CLSA3541"}
 };
 
 struct snd_soc_acpi_mach snd_soc_acpi_amd_sof_machines[] = {
@@ -164,6 +167,15 @@ struct snd_soc_acpi_mach snd_soc_acpi_amd_sof_machines[] = {
 EXPORT_SYMBOL(snd_soc_acpi_amd_sof_machines);
 
 struct snd_soc_acpi_mach snd_soc_acpi_amd_vangogh_sof_machines[] = {
+	{
+		.id = "NVTN2020",
+		.drv_name = "nau8821-cs3",
+		.pdata = &acp_quirk_data,
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &amp_cs35l41,
+		.fw_filename = "sof-vangogh.ri",
+		.sof_tplg_filename = "sof-vangogh-nau8821-cs35l41.tplg",
+	},
 	{
 		.id = "NVTN2020",
 		.drv_name = "nau8821-max",
